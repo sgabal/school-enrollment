e6b4b2a9525135bd8373fc2c103cda6a65624f95
sgabal <cairo89>
2012-09-17 19:37:49 -0500

    Calendar

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -134,6 +134,7 @@ grails.plugins.springsecurity.securityConfigType = "InterceptUrlMap"
 grails.plugins.springsecurity.interceptUrlMap = [
         '/profile/**':   ["hasAnyRole('ROLE_ADMIN', 'ROLE_USER')"],
         '/class/**':   ["hasAnyRole('ROLE_ADMIN', 'ROLE_USER')"],
+        '/calendar/**':   ["hasAnyRole('ROLE_ADMIN', 'ROLE_USER')"],
         '/admin/**':   ["hasAnyRole('ROLE_ADMIN')"],
         '/*':            ["hasAnyRole('ROLE_ADMIN', 'ROLE_USER')"],
         '/login/**':     ['IS_AUTHENTICATED_ANONYMOUSLY'],
