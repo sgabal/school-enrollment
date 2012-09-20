b88d9ed2f03a5fd637df71279c2577d5678ebea5
sgabal <cairo89>
2012-09-19 21:56:30 -0500

    Shared Conf

---
+++ shared/conf/DataSource.groovy
@@ -0,0 +1,24 @@
+dataSource {
+	dbCreate="create"
+	url="jdbc:oracle:thin:@192.168.56.101:1521:xe"
+	username="schoolportal"
+	password="schoolportal"
+	driverClassName = "oracle.jdbc.driver.OracleDriver"
+	dialect="org.hibernate.dialect.Oracle10gDialect"
+	loggingSql = false
+	pooled = true
+	properties {
+		maxActive = 50
+		maxIdle = 25
+		minIdle = 5
+		initialSize = 5
+		minEvictableIdleTimeMillis = 60000
+		timeBetweenEvictionRunsMillis = 60000
+		maxWait = 10000
+		validationQuery = "select 1 from dual"
+	}
+
+}
+
+
+
