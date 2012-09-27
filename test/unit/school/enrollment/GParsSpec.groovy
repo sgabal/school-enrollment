2120dfee0519da44126504000d88cb6f23f3cadc
Sam Gabal <sam.gabal@yahoo.com>
2012-09-27 09:20:03 -0500

    Added GParsSpec

--- test/unit/school/enrollment/GParsSpec.groovy
+++ test/unit/school/enrollment/GParsSpec.groovy
@@ -6,26 +6,22 @@ import spock.lang.Specification
 class GParsSpec extends Specification {
 
     def "Map Reduce"() {
-        given:
-            def result
         when:
-            withPool {
-                result = [0, 1, 2, 3, 4].parallel
-                        .map { it + 1 }
-                        .map { it ** 2 }
-                        .reduce { a, b -> a + b }
+            def result =  withPool {
+                [0, 1, 2, 3, 4].parallel
+                    .map { it + 1 }
+                    .map { it ** 2 }
+                    .reduce { a, b -> a + b }
             }
         then:
             result == 55
     }
 
     def "Fork Join"() {
-        given:
-            def squares
         when:
-            withPool {
+            def squares = withPool {
                 def numbers = [1, 2, 3, 4, 5, 6].makeConcurrent()
-                squares = numbers.collect{ it * it }.grep{ it < 10 }
+                numbers.collect{ it * it }.grep{ it < 10 }
             }
         then:
             squares == [1, 4, 9]
